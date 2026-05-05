fe30a2e687248559ae942d450fef6f15074cb3d2
ismail <ismail960i960@gmail.com>
2026-05-05 17:22:37 +0300

    added fav feature

--- lib/features/fav/presentation/screen/fav.dart
+++ lib/features/fav/presentation/screen/fav.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:talam/features/common/page_view_widget.dart';
 import 'package:talam/features/fav/service/fav_service.dart';
-import 'package:talam/features/home/domain/quran_ayah.dart';
 
 class FavScreen extends ConsumerWidget {
   const FavScreen({super.key});
